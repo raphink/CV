534f236eaa0584e42b0ae307b6afd9b676b5a0df
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-05-12 22:33:32 +0200

    Add skills directory

--- skills/js/skills.js
+++ skills/js/skills.js
@@ -26,63 +26,63 @@ function skills() {
     // 0: Provisioning
     {id: 01, group: 0, content: 'Debian FAI', start: '2006-06-01', end: c2c_start, className: 'contributed'},
     {id: 02, group: 0, content: 'Kickstart', start: '2006-06-01', end: new Date(), className: 'implemented'},
-    {id: 03, group: 0, content: 'Terraform', logo: 'terraform', start: '2016-05-01', end: new Date(), className: 'implemented'},
+    {id: 03, group: 0, content: 'Terraform', name: 'terraform', start: '2016-05-01', end: new Date(), className: 'implemented'},
 
     // 1: Cloud / IAAS
-    {id: 11, group: 1, content: 'Amazon Web Services', logo: 'aws', start: '2010', end: new Date(), className: 'implemented'},
-    {id: 12, group: 1, content: 'Openstack', logo: 'openstack', start: '2013', end: new Date(), className: 'used'},
+    {id: 11, group: 1, content: 'Amazon Web Services', name: 'aws', start: '2010', end: new Date(), className: 'implemented'},
+    {id: 12, group: 1, content: 'Openstack', name: 'openstack', start: '2013', end: new Date(), className: 'used'},
 
     // 2: Virtualization
-    {id: 21, group: 2, content: 'Qemu', logo: 'qemu', start: '2005', end: '2007', className: 'used'},
-    {id: 22, group: 2, content: 'Xen', logo: 'xen', start: '2008', end: c2c_start, className: 'used'},
-    {id: 23, group: 2, content: 'Virtualbox / Vagrant', logo: 'virtualbox', start: c2c_start, end: new Date(), className: 'used'},
+    {id: 21, group: 2, content: 'Qemu', name: 'qemu', start: '2005', end: '2007', className: 'used'},
+    {id: 22, group: 2, content: 'Xen', name: 'xen', start: '2008', end: c2c_start, className: 'used'},
+    {id: 23, group: 2, content: 'Virtualbox / Vagrant', name: 'virtualbox', start: c2c_start, end: new Date(), className: 'used'},
 
     // 3: Operating Systems
-    {id: 31, group: 3, content: 'Debian', logo: 'debian', start: '2002', end: new Date(), className: 'contributed'},
-    {id: 32, group: 3, content: 'Ubuntu', logo: 'ubuntu', start: '2004', end: new Date(), className: 'developed'},
-    {id: 33, group: 3, content: 'RedHat / CentOS', logo: 'redhat', start: '2006', end: new Date(), className: 'implemented'},
+    {id: 31, group: 3, content: 'Debian', name: 'debian', start: '2002', end: new Date(), className: 'contributed'},
+    {id: 32, group: 3, content: 'Ubuntu', name: 'ubuntu', start: '2004', end: new Date(), className: 'developed'},
+    {id: 33, group: 3, content: 'RedHat / CentOS', name: 'redhat', start: '2006', end: new Date(), className: 'implemented'},
 
     // 4: VCS
     {id: 41, group: 4, content: 'CVS', start: '2008', end: c2c_start, className: 'implemented'},
-    {id: 42, group: 4, content: 'Bazaar NG', logo: 'bzr', start: '2005', end: '2008', className: 'used'},
-    {id: 43, group: 4, content: 'Git', logo: 'git', start: '2008', end: new Date(), className: 'implemented'},
+    {id: 42, group: 4, content: 'Bazaar NG', name: 'bzr', start: '2005', end: '2008', className: 'used'},
+    {id: 43, group: 4, content: 'Git', name: 'git', start: '2008', end: new Date(), className: 'implemented'},
 
     // 5: OS coding
     {id: 51, group: 5, content: 'Bash / Dash', start: '2005', end: new Date(), className: 'used'},
-    {id: 52, group: 5, content: 'Perl', logo: 'perl', start: '2006', end: c2c_start, className: 'implemented'},
+    {id: 52, group: 5, content: 'Perl', name: 'perl', start: '2006', end: c2c_start, className: 'implemented'},
     {id: 53, group: 5, content: 'C', start: '2008', end: new Date(), className: 'used'},
-    {id: 54, group: 5, content: 'Ruby', logo: 'ruby', start: c2c_start, end: new Date(), className: 'implemented'},
-    {id: 55, group: 5, content: 'Go', logo: 'go', start: '2016', end: new Date(), className: 'used'},
+    {id: 54, group: 5, content: 'Ruby', name: 'ruby', start: c2c_start, end: new Date(), className: 'implemented'},
+    {id: 55, group: 5, content: 'Go', name: 'go', start: '2016', end: new Date(), className: 'used'},
 
     // 6: Configuration Management
-    {id: 61, group: 6, content: 'Cfengine', logo: 'cfengine', start: '2006', end: c2c_start, className: 'implemented'},
-    {id: 62, group: 6, content: 'Puppet', logo: 'puppet', start: '2007', end: new Date(), className: 'contributed'},
-    {id: 63, group: 6, content: 'Augeas', logo: 'augeas', details: 'augeas', start: '2008', end: new Date(), className: 'developed'},
+    {id: 61, group: 6, content: 'Cfengine', name: 'cfengine', start: '2006', end: c2c_start, className: 'implemented'},
+    {id: 62, group: 6, content: 'Puppet', name: 'puppet', start: '2007', end: new Date(), className: 'contributed'},
+    {id: 63, group: 6, content: 'Augeas', name: 'augeas', start: '2008', end: new Date(), className: 'developed'},
 
     // 7: Containers
     {id: 71, group: 7, content: 'Chroot', start: '2006', end: '2010', className: 'used'},
-    {id: 72, group: 7, content: 'LXC', logo: 'lxc', start: '2008', end: c2c_start, className: 'implemented'},
-    {id: 73, group: 7, content: 'OpenVZ', logo: 'openvz', start: c2c_start, end: '2014', className: 'used'},
-    {id: 74, group: 7, content: 'Docker / Rancher', logo: 'docker', start: '2014', end: new Date(), className: 'implemented'},
+    {id: 72, group: 7, content: 'LXC', name: 'lxc', start: '2008', end: c2c_start, className: 'implemented'},
+    {id: 73, group: 7, content: 'OpenVZ', name: 'openvz', start: c2c_start, end: '2014', className: 'used'},
+    {id: 74, group: 7, content: 'Docker / Rancher', name: 'docker', start: '2014', end: new Date(), className: 'implemented'},
 
     // 8: Databases
-    {id: 81, group: 8, content: 'MySQL', logo: 'mysql', start: '2002', end: new Date(), className: 'implemented'},
-    {id: 82, group: 8, content: 'PostgreSQL', logo: 'postgresql', start: '2007', end: new Date(), className: 'implemented'},
+    {id: 81, group: 8, content: 'MySQL', name: 'mysql', start: '2002', end: new Date(), className: 'implemented'},
+    {id: 82, group: 8, content: 'PostgreSQL', name: 'postgresql', start: '2007', end: new Date(), className: 'implemented'},
 
 
     // 9: Cloud / PAAS
-    {id: 91, group: 9, content: 'Google AppEngine', logo: 'google_appengine', start: '2008', end: '2010', className: 'used'},
+    {id: 91, group: 9, content: 'Google AppEngine', name: 'google_appengine', start: '2008', end: '2010', className: 'used'},
 
     // 10: Web backend
-    {id: 101, group: 10, content: 'PHP', logo: 'php', start: '2002', end: new Date(), className: 'used'},
-    {id: 102, group: 10, content: 'Python', logo: 'python', details: 'python', start: '2008', end: new Date(), className: 'implemented'},
-    {id: 103, group: 10, content: 'Ruby', logo: 'ruby', details: 'ruby', start: '2013', end: new Date(), className: 'implemented'},
-    {id: 104, group: 10, content: 'Go', logo: 'go', details: 'go', start: '2016', end: new Date(), className: 'used'},
+    {id: 101, group: 10, content: 'PHP', name: 'php', start: '2002', end: new Date(), className: 'used'},
+    {id: 102, group: 10, content: 'Python', name: 'python', details: 'python', start: '2008', end: new Date(), className: 'implemented'},
+    {id: 103, group: 10, content: 'Ruby', name: 'ruby', details: 'ruby', start: '2013', end: new Date(), className: 'implemented'},
+    {id: 104, group: 10, content: 'Go', name: 'go', details: 'go', start: '2016', end: new Date(), className: 'used'},
 
     // 11: Web frontend
-    {id: 111, group: 11, content: 'HTML / Javascript / CSS', logo: 'html_css_js', details: "html_css_js", start: '2002', end: new Date(), className: 'implemented'},
-    {id: 112, group: 11, content: 'JQuery', logo: 'jquery', start: c2c_start, end: new Date(), className: 'used'},
-    {id: 113, group: 11, content: 'Bootstrap', logo: 'bootstrap', start: '2015', end: new Date(), className: 'used'},
+    {id: 111, group: 11, content: 'HTML / Javascript / CSS', name: 'html_css_js', details: "html_css_js", start: '2002', end: new Date(), className: 'implemented'},
+    {id: 112, group: 11, content: 'JQuery', name: 'jquery', start: c2c_start, end: new Date(), className: 'used'},
+    {id: 113, group: 11, content: 'Bootstrap', name: 'bootstrap', start: '2015', end: new Date(), className: 'used'},
   ]);
 
   var container = document.getElementById('visualization');
@@ -112,20 +112,11 @@ function skills() {
     orientation: 'both',
     autoResize: true,
     template: function(item) {
-      var title;
-      if (item.logo) {
-        title = '<img src="img/'+item.logo+'.png" class="logo small" /> '+item.content;
-      } else {
-        title = item.content;
-      }
+      var title = '<img src="skills/'+item.name+'/logo.png" class="logo small" onerror="this.parentNode.removeChild(this)" /> '+item.content;
 
-      if (item.details) {
-        var html = '<a class="details-toggle" data-toggle="collapse" href="#details-'+item.id+'">'+title+'</a>';
+      var html = '<a class="details-toggle" data-toggle="collapse" href="#details-'+item.id+'">'+title+'</a>';
         html += '<div class="details collapse" id="details-'+item.id+'"></div>';
-        return html;
-        } else {
-        return title;
-      }
+      return html;
     }
   };
 
@@ -150,7 +141,7 @@ function loadDetails(items) {
   for (var i in items._data) {
     var ii = items._data[i];
     $.ajax({
-        url: './details/'+ii.details+'.html',
+        url: './skills/'+ii.name+'/details.html',
         id: i,
         success: function(details) {
           $('#details-'+this.id).html(details);
